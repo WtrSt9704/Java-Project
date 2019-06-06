c6e5aa5f0f863bc2afe83a0e6135106f505bbda9
WtrSt9704 <50455428+WtrSt9704@users.noreply.github.com>
2019-06-06 15:12:35 +0900

    전상건

--- JuneSixth/src/Menu.java
+++ JuneSixth/src/Menu.java
@@ -1,5 +1,11 @@
 import javax.swing.JFrame;
 
-public class Menu extends JFrame{
+public class Menu extends JFrame  implements ActionListener {
+	private Jbutton Start;
+	private Jbutton record;
+	private Jbutton exit;
+	
+	private Jpanel button;
+	
 	
 }
